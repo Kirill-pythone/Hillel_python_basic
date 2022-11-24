82d6772b5cdc67afebfd910c776f68ec774194aa
Kirill-pythone <118853430+Kirill-pythone@users.noreply.github.com>
2022-11-24 19:43:24 +0200

    Add files via upload

---
+++ Homework 1/main.py
@@ -0,0 +1,13 @@
+print('hello')
+print('import antigravity')
+print('import __hello__')
+print('from __future__ import braces')
+
+print('\nEscape  sequences')
+print('\\a-Bell (alert)')
+print('\\b-Backspace')
+print('\\n-New line')
+print('\\t-Horizontal tab')
+print('\\\ - Backslash')
+print('\\"-Double quotation mark "')
+print("\\'- Single quotation mark '")
