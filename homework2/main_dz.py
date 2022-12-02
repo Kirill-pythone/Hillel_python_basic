572aaa41a3c27542de970722105014dfc6372353
Kirill-pythone <118853430+Kirill-pythone@users.noreply.github.com>
2022-12-02 14:40:28 +0200

    Add files via upload

---
+++ homework2/main_dz.py
@@ -0,0 +1,5 @@
+apple = int(input('Введите колличество яблок: '))
+student = int(input('Введите колличество студентов:'))
+a = apple // student
+print(f'Яблок получит каждый студент {apple // student}')
+print(f'Яблок останется {apple % student}')
