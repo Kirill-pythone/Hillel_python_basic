fafb3cd567fd3c69e63a0e1f807126319ce01f6e
Kirill-pythone <118853430+Kirill-pythone@users.noreply.github.com>
2022-12-04 18:59:08 +0200

    Add files via upload

---
+++ homework2/main_dz_3.py
@@ -0,0 +1,6 @@
+number = int(input('Введите целое трёхзначное число:'))
+print(number % 10 \, number // 10 % 10, number // 100)
+# f'Вот ваше число задом наперёд:'
+# f'{number % 10}'
+# f' {number //10 % 10}'
+# f' {number // 100}')
