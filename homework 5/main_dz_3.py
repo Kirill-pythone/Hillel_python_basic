3335500c29f0c3d16e2edec4cbca3067ac1c6cbd
Kirill-pythone <118853430+Kirill-pythone@users.noreply.github.com>
2022-12-20 19:28:06 +0200

    Add files via upload

---
+++ homework 5/main_dz_3.py
@@ -0,0 +1,9 @@
+my_list = [1, 2]
+print(my_list)
+for i in my_list:
+    if len(my_list) == 2:
+        my_list.append(0)
+        print(my_list)
+    #elif len(my_list) > 2:
+     #   my_list[01] = [0] + [1]
+      #  print(my_list)
