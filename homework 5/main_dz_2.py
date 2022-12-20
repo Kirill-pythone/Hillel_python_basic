cbc8a3673d559dc6a8e45447d4601797c006b0e3
Kirill-pythone <118853430+Kirill-pythone@users.noreply.github.com>
2022-12-20 19:27:53 +0200

    Add files via upload

---
+++ homework 5/main_dz_2.py
@@ -0,0 +1,9 @@
+my_list = [80, 70, 90, 100, 110, 120, 99, 150]
+my_results = []
+for i in my_list:
+    if i > 100:
+        my_results.append(i)
+        print(my_results)
+       # my_results = i
+        #print(my_results)
+#print()
